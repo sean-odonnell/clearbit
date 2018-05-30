d99a9ed8e8f4550f3f751cc13e307b9ebd0c7293
Sean O'Donnell <Home@MacBook.local>
2018-05-30 07:10:59 -0700

    initial commit

---
+++ server/data/api.js
@@ -0,0 +1,13 @@
+// Plaid API Setup
+
+const PLAID_API_REQUEST = {
+    SANDBOX_USER: 'user_good',
+    SANDBOX_PASSWORD: 'pass_good', 
+    APP_PORT: 8000,
+    PLAID_CLIENT_ID: '5b0e587c4404380012f817b7',
+    PLAID_SECRET: '83facd96d5c80cccbdda71cd516a6b',
+    PLAID_PUBLIC_KEY: '6a3ea516ba67c9203185ae9cb38e5f',
+    PLAID_ENV: 'sandbox'
+}
+
+export default PLAID_API_REQUEST; 
